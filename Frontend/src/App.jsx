23452e95ae9f0e3a1f3b297dd75ddd96707ca3e6
FrancoisMartinez <160101566+FrancoisMartinez@users.noreply.github.com>
2025-01-25 13:09:38 -0500

    pages and header

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import Home from './pages/Home.jsx';
 import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import AccountDashboard from "./pages/AccountDashboard.jsx";
+import TestTakingPage from './pages/TestTakingPage.jsx'
 
 const router = createBrowserRouter([
     {path: '/', element: <Home/>},
+    {path: '/account', element: <AccountDashboard/>},
+    {path: '/testTaking', element: <TestTakingPage/>}
 
 ]);
 
