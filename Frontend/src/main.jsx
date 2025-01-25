5168198d97dd73e184e0a3fbe9542c644b384566
antholim <anthonylim459@hotmail.com>
2025-01-25 13:24:52 -0500

    Auth0

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react';
+import authConfig from "./auth_config.json"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+      domain={authConfig.domain}
+      clientId={authConfig.clientId}
+      redirect_uri={window.location.origin}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>,
 )
