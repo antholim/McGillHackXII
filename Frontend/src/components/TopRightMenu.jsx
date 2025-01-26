781fe6a74df169b162e901970b1b2f140da2d38d
antholim <anthonylim459@hotmail.com>
2025-01-25 23:05:26 -0500

    TopRight

--- Frontend/src/components/TopRightMenu.jsx
+++ Frontend/src/components/TopRightMenu.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from './LoginButton'; // Import the LoginButton component
 import LogoutButton from './Logout'; // Import the LogoutButton component
 import "./TopRightMenu.css"; // Import the CSS file
 
 export default function TopRightMenu() {
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
   const navigate = useNavigate();
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  useEffect(()=> {
+      const token = localStorage.getItem('token');
+      if (token) {
+          setIsAuthenticated(true);
+      }
+  }, [isAuthenticated])
   const handleNavigation = (path) => {
     if (isAuthenticated) {
       navigate(path);
@@ -27,7 +32,7 @@ export default function TopRightMenu() {
             <button onClick={() => handleNavigation('/testTaking')}>TRY ME</button>
             <button>FEATURE(S)</button>
             <button onClick={() => handleNavigation('/account')}>ACCOUNT</button>
-            {isAuthenticated ? <LogoutButton /> : <LoginButton />} {/* Add login/logout button */}
+            {isAuthenticated ? <LogoutButton setIsAuthenticated={setIsAuthenticated}/> : <LoginButton setIsAuthenticated={setIsAuthenticated}/>} {/* Add login/logout button */}
           </div>
         )}
       </div>
